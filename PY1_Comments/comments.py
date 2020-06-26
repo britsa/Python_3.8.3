e8484f7c482a33c2998a6a7a14b0c89747caf697
getpushed <askregins@gmail.com>
2020-06-26 19:17:38 +0530

    comments

---
+++ PY1_Comments/comments.py
@@ -0,0 +1,13 @@
+# comments
+
+# This a comment - print('Hello World')
+# The above print() do not work
+
+print('# But this is not a comment') # The statement is enclosed with apostrophes.
+# The above print() will work
+
+'''
+We hope that you understood the usage of comments.
+
+You are seeing this text inside of multi-line comment
+'''
